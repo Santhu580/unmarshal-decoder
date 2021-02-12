df3d38eff51c7c8628d754461e473c46a32286ee
Santhosh-Git <santhoshgeetha98@gmail.com>
2021-02-12 09:49:41 +0530

    fix changes

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/unmarshal-decoder1'));
+app.use(express.static(__dirname,'./dist/unmarshal-decoder1'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/unmarshal-decoder1/'}),
-);
+app.get('/*', function(req,res) {
+
+    if(req.url === '/') {
+      res.sendFile(path.join(__dirname,'/dist/unmarshal-decoder1/index.html'));
+    }
+    else {
+      res.sendFile(path.join(__dirname, '/dist/unmarshal-decoder1' + req.url))
+    }
+  
+  });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
