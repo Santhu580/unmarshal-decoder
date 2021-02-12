255d2ca99c0b54c9d52243a92b342d5db400d662
Santhosh-Git <santhoshgeetha98@gmail.com>
2021-02-12 12:45:48 +0530

    fix changes

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const ngApp = express();
 
-ngApp.use(express.static('./dist/unmarshal-decoder1'));
+ngApp.use(express.static('./dist/unmarshal-decoder'));
 
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/unmarshal-decoder1/index.html'));
+    response.sendFile(path.join(__dirname, '/dist/unmarshal-decoder/index.html'));
 });
 
 ngApp.listen(process.env.PORT || 8080);
